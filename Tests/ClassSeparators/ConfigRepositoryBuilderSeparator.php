729586f7427760a2e20e9113f116e15994527b5e
Bohdan <bohdan.shevchenko2@nure.ua>
2021-12-15 12:47:36 +0200

    Add mock for scopeConfig

--- Tests/ClassSeparators/ConfigRepositoryBuilderSeparator.php
+++ Tests/ClassSeparators/ConfigRepositoryBuilderSeparator.php
@@ -3,11 +3,20 @@
 namespace GingerPay\Payment\Tests\ClassSeparators;
 
 require_once __DIR__.'/../../Redefiners/Model/ModelBuilderRedefiner.php';
+require_once __DIR__.'/../Mocks/ScopeConfig.php';
 
 use GingerPay\Payment\Redefiners\Model\ModelBuilderRedefiner;
+use GingerPay\Payment\Tests\Mocks\ScopeConfig;
 
 class ConfigRepositoryBuilderSeparator extends ModelBuilderRedefiner
 {
+    protected $scopeConfig;
+
+    public function __construct()
+    {
+        $this->scopeConfig = new ScopeConfig();
+    }
+
     public function getStoreConfig(string $path, int $storeId = 0)
     {
         if ('payment/ginger_methods_ideal/description')
@@ -18,10 +27,5 @@ class ConfigRepositoryBuilderSeparator extends ModelBuilderRedefiner
         {
             return 'TestStore';
         }
-    }
-
-    public function getExtensionVersion(): string
-    {
-        return '1.1.0';
     }
 }
